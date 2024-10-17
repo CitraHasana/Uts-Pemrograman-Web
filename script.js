c9e4cd10b8a5945182ac8817f352c9ea9f90b7fa
CitraHasana <ctrhsnaaa01@gmail.com>
2024-10-17 10:37:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  var typed = new Typed(".typing", {
+    strings: ["Students at Information Technology", "Teacher", "Programmer "],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+  });
+});
